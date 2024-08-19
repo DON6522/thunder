7cb2740b0e21f19a1f7d8865e65fc5ff7749b2ae
DON6522 <don309343@gmail.com>
2024-08-19 13:25:37 +0530

    Update test-write.js

--- test-write.js
+++ test-write.js
@@ -1,1 +1,13 @@
 
+const fs = require('fs');
+
+const testFilePath = 'submissions.json';
+const testData = ['Test data'];
+
+fs.writeFile(testFilePath, JSON.stringify(testData, null, 2), (err) => {
+    if (err) {
+        console.error('Failed to write test data:', err);
+    } else {
+        console.log('Test data written successfully');
+    }
+});
